e6fce7c65262374fb2945e7d2be4bfa60bc8421f
Eduardo Oliveira <eduardousp@gmail.com.br>
2024-01-12 17:18:08 -0300

    conection string

--- teste-back-flame/loja/loja.data/Data/Context.cs
+++ teste-back-flame/loja/loja.data/Data/Context.cs
@@ -12,15 +12,8 @@ namespace loja.data.Data
 {
     public class Context:DbContext
     {
-        string conectionStringMySql = "Server=localhost;Port=3306;initial catalog=lojaflame;uid=root;pwd=007987Pq";
-       
-
         public Context(DbContextOptions options):base(options) { }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseMySql(conectionStringMySql, ServerVersion.Parse("8.2.0 - MySQL Community Server"));
-            base.OnConfiguring(optionsBuilder);
-        }
+       
       
         public DbSet<Products> Products { get; set; }
         public DbSet<Sales> Sales { get; set; }
